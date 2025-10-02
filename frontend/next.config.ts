1569dec31f16de6e55616c29eabc4b7f6dcad80b
Alex Kerney <abk@mac.com>
2025-10-02 14:46:31 -0400

    Build frontend
    Currently fails due to missing types from Axiom forms.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  rewrites: async () => {
-    return [
-      {
-        source: "/backend/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/backend/:path*`,
-      },
-    ];
-  },
+  // rewrites: async () => {
+  //   return [
+  //     {
+  //       source: "/backend/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/backend/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
