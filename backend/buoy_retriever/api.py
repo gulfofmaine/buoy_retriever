27af6259a85bdbd7d9b7578373344cc519598f2e
Alex Kerney <abk@mac.com>
2025-12-31 10:15:27 -0500

    Proxy backend through NextJS

--- backend/buoy_retriever/api.py
+++ backend/buoy_retriever/api.py
@@ -2,7 +2,7 @@ from datasets.api import router as datasets_router
 from ninja import NinjaAPI
 from pipelines.api import router as pipelines_router
 
-api = NinjaAPI()
+api = NinjaAPI(docs_url="/docs/")
 
 api.add_router("/datasets/", datasets_router)
 api.add_router("/pipelines/", pipelines_router)
